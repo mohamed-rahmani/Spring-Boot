92ad2bd4bb2bd2131d0a9097fd3526bf74c6e206
mohamed-rahmani <bouchkhaf@gmail.com>
2025-02-20 11:53:52 +0100

    refactorisation du projet

--- src/main/java/com/example/demo/repository/IEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Employee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEmployeeRepository extends CrudRepository<Employee, Long> {
-}
